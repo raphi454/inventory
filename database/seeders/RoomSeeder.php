4f7b5f8652e8da46833ee9695192e0eb56122f6f
rsteger <rsteger@schueler.hakzell.at>
2026-03-05 19:45:22 +0100

    RoomSeeder fertiggestellt -> beim create_rooms timestamps hinzugefügt

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Room;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,24 @@ class RoomSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
-        //
+        $rooms = [[
+            'title' => '3BHK'
+        ],
+            [
+                'title' => '4BHK'
+            ],
+            [
+                'title' => '2BHK'
+            ]
+        ];
+        foreach ($rooms as $room) {
+            $r = new Room();
+            $r->title = $room['title'];
+            $r->school_id = 1; // Schule ID setzen
+            $r->save();
     }
+}
 }
