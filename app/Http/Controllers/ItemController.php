723396365dab3ff103ef3ecfeae4bdb4bc6b03e7
rsteger <rsteger@schueler.hakzell.at>
2026-03-05 19:30:10 +0100

    create RoomSeeder and CategorieSeeder

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
+
     public function index(){
-        return view('items.index');
+
+        $items = Item::all()->where(function($school){
+            return $school->school_id === 101;
+        });
+
+        //dd($items);
+        //dump and die
+
+        return view('items.index',
+        ['items'=>$items]);
+
     }
 }
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,2 +1,10 @@
 
 <h2>{{__('Items')}}</h2>
+
+<div>
+    @foreach($items as $item)
+        <div>
+            {{ $item->invnumber}} {{$item->title}}
+        </div>
+    @endforeach
+</div>
